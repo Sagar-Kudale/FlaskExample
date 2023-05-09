3db06173a4b4ef7ecf398bce878718956a852942
sagar <sagarkudale2014.sk@gmail.com>
2023-05-09 21:41:04 +1000

    Simple Rest API changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello  and Kudale-Sid!'
+    return 'Hello  and Kudale-Sid!!!!'
 
 
 if __name__ == '__main__':

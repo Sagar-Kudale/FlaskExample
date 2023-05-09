84ee86786e4832738e4876255bba18806d96c193
sagar <sagarkudale2014.sk@gmail.com>
2023-05-09 21:32:38 +1000

    Simple Rest API

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello pip and Sagar Kudale!'
+    return 'Hello pip and Sagar Kudale-Sid!'
 
 
 if __name__ == '__main__':
